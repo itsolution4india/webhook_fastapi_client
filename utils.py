03747d21e0acf812107e96ff7f503353eba720ee
Prashanthg01 <prashanthprashanth8585@gmail.com>
2025-03-16 21:36:40 +0530

    changed domain

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import logging
 import os, re
 
 LOGS_DIR = 'message_logs'
-DASHBOARD_URL = "https://wtsdealnow/user_responses/"
+DASHBOARD_URL = "https://wtsdealnow.com/user_responses/"
 
 if not os.path.exists(LOGS_DIR):
     os.makedirs(LOGS_DIR)
